24cf75868f275015c9597358dae30110001b5dda
Sushank Yadav <119575978+Sushankyadav11@users.noreply.github.com>
2025-01-16 11:42:16 +0530

    Delete Day3 directory

--- Day3/logArguments.js
+++
@@ -1,8 +0,0 @@
-// Write a function logArguments that takes any number of arguments and logs them to
-// the console. Use rest parameters to collect the arguments.
-
-function logArguments(...args){
-    console.log(args);
-}
-
-logArguments(1,2,3,4,5);
--- Day3/sum.js
+++
@@ -1,8 +0,0 @@
-// Define a function sum that takes three numbers as parameters and returns their sum. Use
-// the spread operator to call this function with an array of numbers.
-
-function sum(...numbers){
-    return numbers[0] + numbers[1] + numbers[2];
-}
-
-console.log(sum(1, 2, 3));
